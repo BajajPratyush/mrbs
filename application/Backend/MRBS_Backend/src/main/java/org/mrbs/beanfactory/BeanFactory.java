b6515ee5d5df994a4f3c26fcc94305324e17a5c3
Pratyush Bajaj <pratyush.bajaj123@gmail.com>
2024-08-18 18:08:18 +0530

    implemented admin functionalities on BeanFactory

--- application/Backend/MRBS_Backend/src/main/java/org/mrbs/beanfactory/BeanFactory.java
+++ application/Backend/MRBS_Backend/src/main/java/org/mrbs/beanfactory/BeanFactory.java
@@ -1,4 +1,22 @@
 package org.mrbs.beanfactory;
 
+import org.mrbs.controller.AdminController;
+import org.mrbs.dao.impl.AdminDaoImpl;
+import org.mrbs.dao.intf.AdminDaoIntf;
+import org.mrbs.service.impl.AdminService;
+
 public class BeanFactory {
+    private AdminDaoIntf adminDao;
+    private AdminService adminService;
+    private AdminController adminController;
+
+    public BeanFactory(){
+        adminDao = new AdminDaoImpl();
+        adminService = new AdminService(adminDao);
+        adminController = new AdminController(adminService);
+    }
+
+    public AdminController getAdminController() {
+        return adminController;
+    }
 }
